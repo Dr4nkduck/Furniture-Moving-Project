087df034e2ea8c2047b05d5ad3c7d0a1f8d45e62
Trần ĐÌnh Dương <hikori08@gmail.com>
2025-10-01 17:38:44 +0700

    fix layout

---
+++ Furniture-Moving-Project/src/main/java/SWP301/Furniture_Moving_Project/controller/DashboardController.java
@@ -0,0 +1,10 @@
+package SWP301.Furniture_Moving_Project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+
+}
