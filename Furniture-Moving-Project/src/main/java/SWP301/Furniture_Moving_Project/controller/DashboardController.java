ceb75c23e4f899b673933965b08d1f84206a4f4e
Trần ĐÌnh Dương <hikori08@gmail.com>
2025-10-02 11:37:50 +0700

    fix css connection

--- Furniture-Moving-Project/src/main/java/SWP301/Furniture_Moving_Project/controller/DashboardController.java
+++ Furniture-Moving-Project/src/main/java/SWP301/Furniture_Moving_Project/controller/DashboardController.java
@@ -6,5 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DashboardController {
 
+    @GetMapping("/operationadmin/dashboard")
+    public String getDashboardPage() {
+        return "operationadmin/dashboard"; // looks for src/main/resources/templates/admin/dashboard.html
+    }
 
+//    @GetMapping("/operationadmin/orders")
+//    public String getOrdersPage() {
+//        return "operationadmin/orders"; // src/main/resources/templates/admin/orders.html
+//    }
+//
+//    @GetMapping("/operationadmin/products")
+//    public String getProductsPage() {
+//        return "operationadmin/products"; // src/main/resources/templates/admin/products.html
+//    }
 }
